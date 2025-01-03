550a544cb28729a8de21b616027f3431fb698dd6
Edson Rodrigues do Nascimento <edsonrhodesbirth@gmail.com>
2025-01-03 15:49:55 -0300

    Modificação para salvar a duração do filme

--- src/main/java/com/Filmoteca/Sistema/Modelo/Filme.java
+++ src/main/java/com/Filmoteca/Sistema/Modelo/Filme.java
@@ -1,9 +1,6 @@
 package com.Filmoteca.Sistema.Modelo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.Duration;
 
 public class Filme {
@@ -15,14 +12,13 @@ public class Filme {
     private Duration duracao;
 
     // Construtor
-    public Filme(String titulo, int ano, String genero, String duracao) {
+    public Filme(String titulo, int ano, String genero, Duration duracao) {
         this.titulo = titulo;
         this.ano = ano;
         this.genero = genero;
-        this.duracao = Duration.parse(duracao);
+        this.duracao = duracao;
 
-        System.out.println("Inserindo Filme: " + this.titulo + ", " + this.ano + ", " + this.genero + ", " + this.duracao.toString());
-
+        System.out.println("Inserindo Filme: " + this.titulo + ", " + this.ano + ", " + this.genero + ", " + this.duracao);
     }
 
     // Método para exibir informações
@@ -30,7 +26,7 @@ public class Filme {
         System.out.println("Titulo: " + titulo);
         System.out.println("Ano: " + ano);
         System.out.println("Genero: " + genero);
-        System.out.println("Duracao: " + duracao);
+        System.out.println("Duração: " + duracao);
     }
 
     // Método para salvar no banco de dados
@@ -51,12 +47,16 @@ public class Filme {
             stmt.setString(1, this.titulo);
             stmt.setInt(2, this.ano);
             stmt.setString(3, this.genero);
-            stmt.setString(4, this.duracao.toString());
 
+            // Convertendo Duration para o formato INTERVAL
+            String duracaoString = String.format("INTERVAL '%d hours %d minutes'", duracao.toHours(), duracao.toMinutesPart());
+
+            // Usando setObject() para passar a string como INTERVAL
+            stmt.setObject(4, this.duracao, Types.OTHER);
 
             // Executar o comando
             stmt.executeUpdate();
-            System.out.println("Filme \"" + this.titulo + "\" salvo no banco de dados!");
+            System.out.println("Filme \"" + titulo + "\" salvo com sucesso.");
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -66,17 +66,17 @@ public class Filme {
     // Método principal
     public static void main(String[] args) {
 
-
         // Criando instâncias de Filme
-        Filme filme1 = new Filme("Senhor dos Aneis", 2010, "Aventura","PT3H");
-        Filme filme2 = new Filme("Matrix", 2000, "Acao","PT2H30M");
-        Filme filme3 = new Filme("MIB", 2012, "Comedia", "PT2H");
-        Filme filme4 = new Filme("Vingadores", 2012, "acao", "PT2H");
+        Filme filme1 = new Filme("Senhor dos Aneis", 2010, "Aventura", Duration.ofHours(2).plusMinutes(30));
+        Filme filme2 = new Filme("Matrix", 2000, "Acao", Duration.ofHours(1).plusMinutes(30));
+        Filme filme3 = new Filme("MIB", 2012, "Comedia", Duration.ofHours(1).plusMinutes(25));
+        Filme filme4 = new Filme("Vingadores", 2012, "acao", Duration.ofHours(1).plusMinutes(12));
 
         // Exibir as informações dos filmes
         filme1.exibirInformacoes();
         filme2.exibirInformacoes();
         filme3.exibirInformacoes();
+        filme4.exibirInformacoes();
 
         // Salvar os filmes no banco de dados
         filme1.salvarNoBanco();
